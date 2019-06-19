88d4ecd36b8b95c64438c5a067e341b66d6ecad2
KrystalCW <krystal.wiederrich@gmail.com>
2019-06-18 19:05:04 -0700

    added path for mongodb to server.js

--- server.js
+++ server.js
@@ -15,8 +15,12 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist";
+
+mongoose.connect(MONGODB_URI);
+
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
 app.listen(PORT, function() {
